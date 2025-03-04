f4ec7b0aaa239931ae62ccea4940aaac21e4a8be
Gibran Maulana <122756881+Khazior@users.noreply.github.com>
2025-03-04 11:38:35 +0800

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,12 +1,10 @@
-// Create web Server
+// Create web server
 const express = require('express');
 const app = express();
 const port = 3000;
-
 app.get('/comments', (req, res) => {
     res.json({ comments: ['comment 1', 'comment 2', 'comment 3'] });
 });
-
 app.listen(port, () => {
     console.log(`Comments service is listening at http://localhost:${port}`);
 });
